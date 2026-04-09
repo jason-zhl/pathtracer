28501250d9db39e286f40ad8a2e370e6c243b925
Jason Li <jason.zhli8@gmail.com>
2026-04-09 04:34:24 -0400

    refactor: moved camera into class

---
+++ src/camera.h
@@ -0,0 +1,77 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include <fstream>
+#include "color.h"
+#include "ray.h"
+#include "vec3.h"
+#include "world.h"
+
+class camera {
+  public:
+    camera(int image_width, double aspect_ratio)
+      : image_width_(image_width) {
+      image_height_ = static_cast<int>(image_width_ / aspect_ratio);
+
+      const auto viewport_height = 9.0;
+      const auto viewport_width = viewport_height * (double(image_width_) / image_height_);
+      const auto focal_length = 4.5;
+
+      center_ = vec3(0, 2, 0);
+
+      const auto viewport_horizontal = vec3(viewport_width, 0, 0);
+      const auto viewport_vertical = vec3(0, viewport_height, 0);
+
+      pixel_delta_horizontal_ = viewport_horizontal / image_width_;
+      pixel_delta_vertical_ = viewport_vertical / image_height_;
+
+      const auto viewport_upper_left = center_ + vec3(0, 0, focal_length)
+        - viewport_horizontal / 2 + viewport_vertical / 2;
+      pixel00_ = viewport_upper_left + 0.5 * (pixel_delta_horizontal_ + pixel_delta_vertical_);
+    }
+
+    int image_width() const { return image_width_; }
+    int image_height() const { return image_height_; }
+
+    void render(const world& scene, std::ofstream* out) const {
+      if (out == nullptr) {
+        return;
+      }
+
+      *out << "P3\n" << image_width_ << " " << image_height_ << "\n255\n";
+
+      for (auto j{0}; j < image_height_; j++) {
+        for (auto i{0}; i < image_width_; i++) {
+          const auto pixel_center = pixel00_ + (i * pixel_delta_horizontal_) - (j * pixel_delta_vertical_);
+          const ray r(center_, pixel_center - center_);
+
+          color pixel_color(0, 0, 0);
+          intersection isect;
+          if (scene.hit(r, isect)) {
+            const vec3 n = unit_vector(isect.surface->normal(isect.point));
+            pixel_color = 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
+          } else {
+            pixel_color = ray_colour(r);
+          }
+
+          write_color(*out, pixel_color);
+        }
+      }
+    }
+
+  private:
+    color ray_colour(const ray& r) const {
+      const vec3 unit_direction = unit_vector(r.direction());
+      const auto a = 0.5 * (unit_direction.y() + 1.0);
+      return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+    }
+
+    int image_width_ = 0;
+    int image_height_ = 0;
+    vec3 center_;
+    vec3 pixel00_;
+    vec3 pixel_delta_horizontal_;
+    vec3 pixel_delta_vertical_;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,13 @@
 #include <fstream>
-#include <iostream>
 #include <memory>
-#include "color.h"
+#include "camera.h"
 #include "plane_patch.h"
-#include "ray.h"
 #include "sphere.h"
 #include "vec3.h"
 #include "world.h"
 
-color ray_colour(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
-    return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
-}
-
 int main() {
-    int width = 800;
-    auto aspect_ratio = 16.0 / 9.0;
-    int height = static_cast<int>(width / aspect_ratio);
-
-    auto viewport_height = 9.0;
-    auto viewport_width = viewport_height * (double(width) / height);
-    auto focal_length = 4.5;
-
-    auto camera_center = vec3(0, 2, 0);
-
-    auto viewport_horizontal = vec3(viewport_width, 0, 0);
-    auto viewport_vertical = vec3(0, viewport_height, 0);
-
-    auto pixel_delta_horizontal = viewport_horizontal / width;
-    auto pixel_delta_vertical = viewport_vertical / height;
-
-    auto viewport_upper_left = camera_center + vec3(0, 0, focal_length) - viewport_horizontal / 2 + viewport_vertical / 2;
-    auto pixel00 = viewport_upper_left + 0.5 * (pixel_delta_horizontal + pixel_delta_vertical);
+    camera cam(800, 16.0 / 9.0);
 
     world scene;
     scene.add(std::make_shared<plane_patch>(
@@ -43,25 +18,7 @@ int main() {
     scene.add(std::make_shared<sphere>(vec3(-4, 2, 20), 6));
 
     std::ofstream ppm_out("image.ppm");
-    ppm_out << "P3\n" << width << " " << height << "\n255\n";
-
-    for (auto j{0}; j < height; j++) {
-        for (auto i{0}; i < width; i++) {
-            auto pixel_center = pixel00 + (i * pixel_delta_horizontal) - (j * pixel_delta_vertical);
-            auto ray_direction = pixel_center - camera_center;
-            ray r(camera_center, ray_direction);
-
-            color pixel_color(0, 0, 0);
-            intersection isect;
-            if (scene.hit(r, isect)) {
-                vec3 n = unit_vector(isect.surface->normal(isect.point));
-                pixel_color = 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
-            } else {
-                pixel_color = ray_colour(r);
-            }
-            write_color(ppm_out, pixel_color);
-        }
-    }
+    cam.render(scene, &ppm_out);
     ppm_out.close();
 
     return 0;
